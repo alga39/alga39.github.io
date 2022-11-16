7487eaf7a65ae9deb940ab13ad69ecd9e1b9ee94
alga39 <100303762+alga39@users.noreply.github.com>
2022-11-17 00:30:53 +0200

    Add files via upload

---
+++ test/header.php
@@ -0,0 +1,24 @@
+<div class="header">
+  <div class="row">
+    <div class="col-md-2 col-sm-2 col-xs-2">
+      <div class="logo">
+        <a href="index.php">
+          <img src="img/bg_lic_head.png" alt="logo" title="МАО лицей 35 им. Буткова В.В.">
+        </a>  
+      </div>
+    </div>
+    <div class="col-md-5 col-sm-5 col-xs-5">
+      <p class="header_txt">
+        Муниципальное автономное общеобразовательное учреждение лицей 35 имени Буткова В.В.
+      </p>
+    </div>
+    <div class="col-md-5 col-sm-5 col-xs-5">
+      <div class="header_txt">
+        236006 г. Калининград, ул. Маршала Баграмяна 34 <br>+7 (4012) 46-98-51 maoulic35@edu.klgd.ru
+      </div>
+      <div class="header_img">
+        <img id="specialButton" style="cursor:pointer;" src="img/special.png" alt="ВЕРСИЯ ДЛЯ СЛАБОВИДЯЩИХ" title="ВЕРСИЯ ДЛЯ СЛАБОВИДЯЩИХ">
+      </div>
+    </div>
+  </div> 
+</div>
