5e8dcaae5c099d575b4d92d1e85a00859acf0771
alga39 <100303762+alga39@users.noreply.github.com>
2022-11-17 00:32:06 +0200

    Add files via upload

---
+++ test/nav.php
@@ -0,0 +1,129 @@
+<nav class="dws-menu">
+  <input type="checkbox" name="toggle" id="menu" class="toggleMenu">
+    <label for="menu" class="toggleMenu">
+      <i class="fa fa-bars"></i>
+      Меню
+    </label>
+    <ul>
+      <li>
+        <a href="#"><i class="fa fa-solid fa-home"></i>
+          Главная
+        </a>
+      </li>
+      <li>
+        <input type="checkbox" name="toggle" id="sub_m2" class="toggleSubmenu">
+          <a href="#">
+            Сведения об ОО
+          </a>
+          <label for="sub_m2" class="toggleSubmenu"><i class="fa"></i></label>
+          <ul>
+            <li>
+              <a href="sveden/common/common.php">Основные сведения</a>
+            </li>
+            <li>
+              <a href="sveden/struct/struct.php">Структура и органы управления</a>
+            </li>
+            <li>
+              <a href="sveden/document/document.php">Документы</a>
+            </li>
+            <li><a href="sveden/education/education.php">Образование</a></li>
+            <li><a href="sveden/employees/employees.php">Руководство, педагогический состав</a></li>
+            <li>
+              <a href="sveden/objects/objects.php">Материално-техническое обеспечение</a>
+            </li>
+            <li>
+              <a href="sveden/paid_edu/paid_edu.php">Платные образовательные услуги</a>
+            </li>
+            <li>
+              <a href="sveden/budget/budget.php">Финансово-хозяйственная деятельность</a>
+            </li>
+            <li>
+              <a href="sveden/vacant/vacant.php">Вакантные места для приема-перевода</a>
+            </li>
+            <li>
+              <a href="sveden/eduStandarts/eduStandarts.php">Образовательные стандарты</a>
+            </li>
+            <li>
+              <a href="sveden/grants/grants.php">Стипендии и иные виды мат.поддержки</a>
+            </li>
+            <li>
+              <a href="sveden/ovz/ovz.php">Доступная среда</a>
+            </li>
+            <li>
+              <a href="sveden/inter/inter.php">Международное сотрудничество</a>
+            </li>
+            <li><a href="sveden/publichn_doklad/publichn_doklad.php">Публичный доклад директора</a>
+            </li>
+            <li>
+              <a href="sveden/vakansii/vakansii.php">Вакансии</a></li>
+        </ul>
+      </li>
+      <li>
+        <input type="checkbox" name="toggle" id="sub_m3" class="toggleSubmenu"> 
+        <a href="#"><!-- <i class="fa fa-solid fa-user"></i> -->Жизнь лицея</a>
+        <label for="sub_m3" class="toggleSubmenu"><i class="fa"></i></label>
+        <ul>
+          <li>
+            <a href="news/all_news_1.php">Новости</a>
+          </li>
+          <li><a href="zhizn_liceya/school_sport_club/school_sport_club.php">Школьный спортивный клуб</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <input type="checkbox" name="toggle" id="sub_m4" class="toggleSubmenu">
+        <a href="#"><!-- <i class="fa fa-solid fa-phone"></i> -->Учебный процесс</a>
+        <label for="sub_m4" class="toggleSubmenu"><i class="fa"></i></label>
+        <ul>
+          <li>
+            <a href="edu-process/vd/vd.php">Внеурочная деятельность</a>
+          </li>
+          <li>
+            <a href="edu-process/gia/gia.php">ГИА</a>
+          </li>
+          <li>
+            <a href="edu-process/vosh/vosh.php">Всероссийская олимпиада школьников</a>
+          </li>
+          <li>
+            <a href="edu-process/raspisanie/raspisanie.php">Расписание уроков</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <input type="checkbox" name="toggle" id="sub_m6" class="toggleSubmenu">
+        <a href="#"><!-- <i class="fa fa-solid fa-phone"></i> -->О лицее</a>
+        <label for="sub_m6" class="toggleSubmenu"><i class="fa"></i></label>
+        <ul>
+          <li><a href="o_shkole/inform_bezopasn/inform_bezopasn.php">Информационная безопасность</a>
+          </li>
+          <li><a href="o_shkole/bezopasn/bezopasn.php">Безопасность</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <input type="checkbox" name="toggle" id="sub_m5" class="toggleSubmenu">
+        <a href="#"><!-- <i class="fa fa-solid fa-phone"></i> -->Родителям</a>
+        <label for="sub_m5" class="toggleSubmenu"><i class="fa"></i></label>
+        <ul>
+          <li><a href="roditelyam/postuplenie/postuplenie.php">Поступление в лицей</a>
+          </li>
+          <li><a href="roditelyam/noko/noko.php">Независимая оценка качества</a>
+          </li>
+          <li><a href="roditelyam/forma/forma.php">Форма</a>
+          </li>
+          <li><a href="roditelyam/transp_karta/transp_karta.php">Транспортная карта</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <input type="checkbox" name="toggle" id="sub_m7" class="toggleSubmenu">
+        <a href="#"><!-- <i class="fa fa-solid fa-envelope"></i> -->Учащимся</a>
+        <label for="sub_m7" class="toggleSubmenu"><i class="fa"></i></label>
+        <ul>
+          <li><a href="uchashimsya/sovety_psichologa/sovety_psichologa.php">Советы психолога</a>
+          </li>
+        </ul>
+      </li>
+      <li><a href="kontakty/kontakty.php">Контакты</a></li>
+    </ul>
+</nav>
