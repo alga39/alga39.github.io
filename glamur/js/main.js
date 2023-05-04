104ae4c242742f7405855edac7c15b7c8035e30c
alga39 <100303762+alga39@users.noreply.github.com>
2023-05-04 02:15:03 +0200

    Add files via upload

---
+++ glamur/js/main.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector('.menu_btn');
+const menu=document.querySelector('.menu_list');
+menuBtn.addEventListener('click', () => {
+	menu.classList.toggle('menu_list_active');
+});
