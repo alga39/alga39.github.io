8d9cd594a43dbf5178f67c24da362caae0faa7f5
alga39 <100303762+alga39@users.noreply.github.com>
2023-06-17 23:42:26 +0200

    Add files via upload

--- mebel/js/main.js
+++ mebel/js/main.js
@@ -15,5 +15,23 @@ function open(evt){
 	});
 	tabTarget.classList.add('category_btn_item_active');
 	document.querySelector(`#${button}`).classList.add('category_box_item_active');
+}
 
+'use strict'
+const newsItem = document.querySelectorAll('.news_btn_item');
+const newsContent = document.querySelectorAll('.news_list');
+newsItem.forEach(function(element){
+	element.addEventListener('click', newsOpen);
+});
+function newsOpen(newsEvt){
+	const newsTarget = newsEvt.currentTarget;
+	const newsButton = newsTarget.dataset.button;
+	newsItem.forEach(function(newsItem){
+		newsItem.classList.remove('news_btn_item_active');
+	});
+	newsContent.forEach(function(newsItem){
+		newsItem.classList.remove('news_list_active');
+	});
+	newsTarget.classList.add('news_btn_item_active');
+	document.querySelector(`#${newsButton}`).classList.add('news_list_active');
 }
