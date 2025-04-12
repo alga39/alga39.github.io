f8d8cad8467a05e8619eadfcd7a1ab794bce1063
Alexandr <100303762+alga39@users.noreply.github.com>
2025-04-12 16:01:00 +0200

    Add files via upload

--- bird_liz/js/main.js
+++ bird_liz/js/main.js
@@ -13,3 +13,20 @@ menuCloce.addEventListener('click', ()=>{
 	menuShadow.classList.remove('menu-open')
 })
 
+const tabItem = document.querySelectorAll('.tabs_btn_items');
+const tabContent = document.querySelectorAll('.tabs_content_items');
+tabItem.forEach(function(element){
+  element.addEventListener('click', open);
+})
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+  tabItem.forEach(function(item){
+    item.classList.remove('tabs_btn_items_active');
+  });
+  tabTarget.classList.add('tabs_btn_items_active');
+  tabContent.forEach(function(item) {
+    item.classList.remove('tabs_content_items_active');
+  });
+  document.querySelector(`#${button}`).classList.add('tabs_content_items_active');
+}
