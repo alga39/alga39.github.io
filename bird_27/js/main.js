95c5ed7a8ebd9109193c93e8b8c7405fec6420f2
Alexandr <100303762+alga39@users.noreply.github.com>
2025-04-12 16:02:33 +0200

    Add files via upload

--- bird_27/js/main.js
+++ bird_27/js/main.js
@@ -13,3 +13,37 @@ menuCloce.addEventListener('click', ()=>{
 	menuShadow.classList.remove('menu-open')
 })
 
+
+
+/*const menuBtn=document.querySelector('.menu_btn')
+const menuCloce=document.querySelector('.menu_close_btn')
+const menulist=document.querySelector('.menu')
+const menuShadow=document.querySelector('.menu-close')
+
+
+menuBtn.addEventListener('click', ()=>{
+	menulist.classList.toggle('menu_list_open')
+	menuShadow.classList.toggle('menu-open')
+});
+menuCloce.addEventListener('click', ()=>{
+	menulist.classList.remove('menu_list_open')
+	menuShadow.classList.remove('menu-open')
+})
+*/
+const tabItem = document.querySelectorAll('.tabs_btn_items');
+const tabContent = document.querySelectorAll('.tabs_content_items');
+tabItem.forEach(function(element){
+  element.addEventListener('click', open);
+})
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+  tabItem.forEach(function(item){
+    item.classList.remove('tabs_btn_items_active');
+  });
+  tabTarget.classList.add('tabs_btn_items_active');
+  tabContent.forEach(function(item) {
+    item.classList.remove('tabs_content_items_active');
+  });
+  document.querySelector(`#${button}`).classList.add('tabs_content_items_active');
+}
